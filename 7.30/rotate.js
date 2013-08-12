0fcd91493e40e2ec5b32b66b37b13191f0027aef
agovizer <michael.mx.xu@gmail.com>
2013-08-12 13:32:29 -0700

    8.12

---
+++ 7.30/rotate.js
@@ -0,0 +1,17 @@
+// Rotate an object around an arbitrary axis in world space       
+function rotateAroundWorldAxis(object, axis, radians) {
+    var rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+
+    // old code for Three.JS pre r54:
+    //  rotWorldMatrix.multiply(object.matrix);
+    // new code for Three.JS r55+:
+    rotWorldMatrix.multiply(object.matrix);                // pre-multiply
+
+    object.matrix = rotWorldMatrix;
+
+    // old code for Three.js pre r49:
+    // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+    // new code for Three.js r50+:
+    object.rotation.setEulerFromRotationMatrix(object.matrix);
+}
