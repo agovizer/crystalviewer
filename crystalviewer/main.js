4b0b91f19edd3fcfe58fd84619fe07ffa5aa1d90
Jiao Lin <linjiao@ubuntu.(none)>
2013-08-24 19:22:05 -0700

    refactor: moved functions in index.html to new file main.js

---
+++ crystalviewer/main.js
@@ -0,0 +1,43 @@
+function build(){
+    rez = 50 //OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO prompt ("Enter sphere resolution"); //number of vertices being rendered, the less, the less (c/g)pu cycles used
+    var rad =5; //prompt ("Radius"); // radius of spheres
+    //prompt ("Dialation Factor"); //obsolete
+    crystalbuild(rez,rad);
+    if (typeof(bases) !== 'undefined') {
+	console.log("bases array exists");
+	basebuild();
+    }
+    else{
+	console.log("bases array does not exist");
+    }
+    //see input.js
+
+    //below is a related to getcoor.js
+
+    //end read from file
+    //}
+    /*else{
+      alert("No selection made");
+      }*/
+}
+
+function light(){
+    amblight = new THREE.AmbientLight( 0x222222 );
+    scene.add( amblight );
+    dirlight = new THREE.DirectionalLight( 0xffffff );
+    dirlight.position.set( 1, 1, 1 );
+    scene.add( dirlight );
+    dirlight = new THREE.DirectionalLight( 0x002288 );
+    dirlight.position.set( -1, -1, -1 );
+    scene.add( dirlight );
+}
+
+
+function animate() {
+    requestAnimationFrame( animate );
+    controls.update();
+}
+function render() {
+    console.log("Renderfunction was called"); // disable to prevent consolespam
+    renderer.render( scene, camera );
+}
